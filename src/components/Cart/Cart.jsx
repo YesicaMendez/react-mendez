a690e34d5d93dd358ba43ffa7d1cfdc1aa1a239d
YesicaMendez <mendezyesica2408@gmail.com>
2022-07-19 21:52:24 -0300

    Proyecto Final React- Arreglos

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,9 @@
 import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
-import '../Cart/Cart.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import "../Cart/Cart.css";
 
 function Cart() {
   const { cart, removeItem, clearCart, getCartTotal } = useContext(CartContext);
@@ -12,8 +12,77 @@ function Cart() {
   return (
     <>
       <div className="row">
-        <div className="offset-md-1 col-md-10 mt-4">
-          <div className="card">
+        <div className="offset-md-1 col-md-10 offset-lg-2 col-lg-8 mt-4">
+          <div className="row g-2">
+            <div className="offset-1 col-10 offset-md-0 col-md-9">
+              <div className="card">
+                <div className="card-header text-center">
+                  <h2 className="fs-4 text">Carrito</h2>
+                </div>
+                <div className="card-body">
+                  {(carritoVacio && (
+                    <div>
+                      <h4>No hay productos en el carrito</h4>
+                    </div>
+                  )) ||
+                    cart.map((item) => (
+                      <div className="row g-1 align-items-center mb-3 pb-2 border-bottom border-warning">
+                        <div className="col-3">
+                          <img
+                            className="cart-item-img img-responsive"
+                            src={item.pictureUrl}
+                            alt={item.title}
+                          />
+                        </div>
+                        <div className="col-8">
+                          <div className="itemCart-title mb-2">
+                            <p className="fw-bold m-0">{item.title}</p>
+                          </div>
+                          <p className="m-0">Cantidad: {item.quantity}</p>
+                          <p className="m-0">Precio $ {item.price}</p>
+                          <p className="m-0 fw-bold">Total= $ {item.quantity * item.price}</p>
+                        </div>
+                        <div className="col-1">
+                          <button
+                            type="button"
+                            className="btn btn-danger btn-sm"
+                            onClick={() => removeItem(item)}
+                          >
+                            <FontAwesomeIcon icon={faTrash} />
+                          </button>
+                        </div>
+                      </div>
+                    ))}
+                  {!carritoVacio &&
+                    <button className="btn btn-warning float-end" onClick={() => clearCart()}>Vaciar Carrito</button>
+                  }
+                </div>
+              </div>
+            </div>
+            <div className="offset-1 col-10 offset-md-0 col-md-3 d-flex justify-content-center">
+              {carritoVacio ?
+                <Link to={`/`}><button className="btn btn-info">Volver al inicio</button></Link>
+                :
+                <div className="card col-10 border-0 mt-2">
+                  <div className="card-header text-center">
+                    <p className="fs-5 fw-bold pb-0 mb-0">Resumen</p>
+                  </div>
+                  <div className="mt-3">
+                    <p className="text-center mb-3 fs-6 fw-bold">Total a pagar: $ {getCartTotal()}</p>
+                  </div>
+                  <div className="text-center">
+                    <Link to={`/checkout/`}><button className="btn btn-info">Continuar al Pago</button></Link>
+                  </div>
+                  <div className="text-center mt-3">
+                    <Link to={`/`}><button className="btn btn-success">Seguir comprando</button></Link>
+                  </div>
+                </div>
+              }
+            </div>
+          </div>
+        </div>
+
+        {/* <div className="card">
             <div className="card-header text-center">
               <h2 className="fs-4 text">Carrito</h2>
             </div>
@@ -56,8 +125,8 @@ function Cart() {
                           </td>
                         </tr>))}
                   </tbody>
-                </table>
-                <div className="d-flex justify-content-center">
+                </table> */}
+        {/* <div className="d-flex justify-content-center">
                   {carritoVacio ?
                     <Link to={`/`}><button className="btn btn-info">Volver al inicio</button></Link>
                     :
@@ -68,12 +137,12 @@ function Cart() {
                       <button className="btn btn-warning ms-4" onClick={() => clearCart()}>Vaciar Carrito</button>
                     </div>
                   }
-                </div>
-              </div>
-            </div>
+                </div> */}
+      </div>
+      {/* </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
